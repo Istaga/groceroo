43e67bbd3dd90225d3fb43ff05528a0fd5b660dd
istaga <imadethislastnight@gmail.com>
2021-09-10 10:36:40 -0400

    merged provider main tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+## Specifies the Region your Terraform Provider will server
+provider "aws" {
+  region = "us-east-1"
+}
+
+## Specifies the S3 Bucket and DynamoDB table used for the durable backend and state locking
+terraform {
+    backend "s3" {
+      encrypt = true
+      bucket = "groceroo-tfstate"
+      dynamodb_table = "terraform-state-lock-dynamo-groceroo"
+      key = "path/path/terraform.tfstate"
+      region = "us-east-1"
+  }
+}
+
+
 # 1. Create VPC
 resource "aws_vpc" "tut_vpc" {
     cidr_block = "10.0.0.0/16"
